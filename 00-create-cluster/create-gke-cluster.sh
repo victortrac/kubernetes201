d511559eea9029f6d717309698e9591db9739b2b
Eric Gonczeruk <eric@gonczeruk.com>
2018-05-11 14:43:09 -0500

    fixed regional cluster response error

--- 00-create-cluster/create-gke-cluster.sh
+++ 00-create-cluster/create-gke-cluster.sh
@@ -17,7 +17,7 @@ gcloud beta container --project "kubernetes201" clusters create "cluster-1" \
   --no-enable-basic-auth \
   --num-nodes "1" \
   --preemptible \
-  --region "us-central1" \
+  --zone "us-central1-a" \
   --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
   --subnetwork "default" \
 
